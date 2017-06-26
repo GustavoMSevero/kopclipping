67a7b8d29663f2c78a8207ef3371d4a27c73fde2
Tecnico - TI <tecnico@epfserver.com.br>
2017-06-26 20:33:48 -0300

    Inicio do trabalho com exemplo

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var TwitterPackage = require('twitter');
 
 var secret = {
-  consumer_key: 'YOURS',
-  consumer_secret: 'YOURS',
-  access_token_key: 'YOURS',
-  access_token_secret: 'YOURS'
+  consumer_key: 'iWExWHtpT49bV5ije1XwnJ6f8',
+  consumer_secret: 'QAyWUBEFZFiMLoyD6neKcHFvaRkZgWJ8peGloR6W6hhdY6KLEd',
+  access_token_key: '36774260-MPr3F8jCLNKFPN1J7f3gS8zknjBVr36NeVwlyiUVV',
+  access_token_secret: '1Az2u0DmNh6ro3n9hXqbjxmwexOVl9ePbjutYrpNN5UT9'
 }
 
 var Twitter = new TwitterPackage(secret);
 
-Twitter.stream('statuses/filter', {track: '@kopzinksi'}, function(stream) {
+Twitter.stream('statuses/filter', {track: '@realDonaldTrump'}, function(stream) {
   stream.on('data', function(tweet) {
     console.log(tweet.text);
   });
